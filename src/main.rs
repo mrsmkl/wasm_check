bbe533c638aff57210df9f67695b88a0ad207bfe
Sami Mäkelä <mrsmkl@gmail.com>
2018-05-15 11:17:52 +0300

    changing offset

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
 extern crate parity_wasm;
 
-fn main() {
-    let module = parity_wasm::deserialize_file("./input.wasm").unwrap();
-    assert!(module.code_section().is_some());
+use parity_wasm::{elements, builder};
 
-    let code_section = module.code_section().unwrap(); // Part of the module with functions code
 
-    println!("Function count in wasm file: {}", code_section.bodies().len());
+
+fn convert_init(expr : &elements::InitExpr) -> elements::InitExpr {
+    let vec = expr.code().to_vec();
+    elements::InitExpr::new(vec)
 }
+
+// 
+fn main() {
+    let mut module = parity_wasm::deserialize_file("input.wasm").unwrap();
+    assert!(module.code_section().is_some());
+    {
+        let code_section = module.code_section().unwrap(); // Part of the module with functions code
+
+        let data_section = module.data_section().unwrap();
+
+        println!("Function count in wasm file: {}", code_section.bodies().len());
+        println!("Segment count in wasm file: {}", data_section.entries().len());
+    }
+    
+    {
+        // let mut data_section = module.data_section_mut().unwrap();
+        // let mut entries = data_section.entries_mut();
+        
+        for data_entry in module.data_section_mut().unwrap().entries_mut() {
+            *data_entry = elements::DataSegment::new(0, convert_init(data_entry.offset()), data_entry.value().to_vec())
+        }
+        
+    }
+    
+    parity_wasm::serialize_to_file("output.wasm", module).expect("Module serialization to succeed");
+}
+
